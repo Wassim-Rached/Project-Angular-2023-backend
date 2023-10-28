8d27cdc571aa26ba0387cfb38dfa43a07639544c
Wassim-Rached <wassimrached404@gmail.com>
2023-10-28 13:00:05 +0100

    complete activites and categories viewsets

--- activities/serializers.py
+++ activities/serializers.py
@@ -1,73 +1,152 @@
 from rest_framework import serializers
-from .models import Category,Activity,ActivityRegistration
-from accounts.serializers import AccountSerializer
+from .models import Category, Activity, ActivityRegistration
+from accounts.serializers import SimpleAccountSerializer
 
+
+# Categories Serializers
 class CategoriesSerializer(serializers.ModelSerializer):
-	class Meta:
-		model = Category
-		fields = '__all__'
+    class Meta:
+        model = Category
+        fields = "__all__"
+
+    def create(self, validated_data):
+        name = validated_data["name"]
+        categorie = Category.objects.filter(name=name).first()
+
+        if categorie:
+            return categorie
+        else:
+            return super().create(validated_data)
+
+
+class SimpleCategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ["name"]
+
+
+# Activities Serializers
+class UpdateActivitiesSerializer(serializers.ModelSerializer):
+    categories = SimpleCategorySerializer(many=True)
+
+    class Meta:
+        model = Activity
+        fields = [
+            "categories",
+            "title",
+            "photo",
+            "is_free",
+            "price",
+            "description",
+            "location",
+            "max_participants",
+            "date",
+        ]
+
+    def update(self, instance, validated_data):
+        # Update fields on the Activity instance
+        instance.title = validated_data.get("title", instance.title)
+        instance.photo = validated_data.get("photo", instance.photo)
+        instance.is_free = validated_data.get("is_free", instance.is_free)
+        instance.price = validated_data.get("price", instance.price)
+        instance.description = validated_data.get("description", instance.description)
+        instance.location = validated_data.get("location", instance.location)
+        instance.max_participants = validated_data.get(
+            "max_participants", instance.max_participants
+        )
+        instance.date = validated_data.get("date", instance.date)
+
+        print(validated_data["categories"])
+
+        # Update categories (Many-to-Many relationship)
+        if "categories" in validated_data:
+            updated_categories = []
+            for category_data in validated_data["categories"]:
+                category, created = Category.objects.get_or_create(
+                    name=category_data["name"]
+                )
+                updated_categories.append(category)
+            instance.categories.set(updated_categories)
+
+        instance.save()
+        return instance
 
 
 class DetailActivitiesSerializer(serializers.ModelSerializer):
-	number_of_likes = serializers.ReadOnlyField()
-	have_ended = serializers.ReadOnlyField()
-	categories = CategoriesSerializer(many=True)
-	# activity_registrations = NonAdminActivityRegistrationSerializer(many=True, read_only=True)
+    number_of_likes = serializers.ReadOnlyField()
+    categories = SimpleCategorySerializer(many=True)
+    posted_by = SimpleAccountSerializer(many=False)
+    likes = SimpleAccountSerializer(many=True)
 
-	class Meta:
-		model = Activity
-		fields = '__all__'
-
-	def create(self, validated_data):
-		# Extract category data from validated_data
-		categories_data = validated_data.pop('categories', [])
-
-		# Create the activity
-		activity = Activity.objects.create(**validated_data)
-
-		# Create and associate categories with the activity
-		for category_data in categories_data:
-			category, created = Category.objects.get_or_create(name=category_data['name'])
-			activity.categories.add(category)
-
-		return activity
+    class Meta:
+        model = Activity
+        fields = "__all__"
 
 
 class ListActivitiesSerializer(serializers.ModelSerializer):
-	number_of_likes = serializers.ReadOnlyField()
-	categories = CategoriesSerializer(many=True,read_only=True)
+    number_of_likes = serializers.ReadOnlyField()
+    categories = SimpleCategorySerializer(many=True, read_only=True)
 
-	class Meta:
-		model = Activity
-		fields = ['id','number_of_likes','categories','title','photo','is_free']
+    class Meta:
+        model = Activity
+        fields = [
+            "id",
+            "title",
+            "photo",
+            "is_free",
+            "number_of_likes",
+            "categories",
+            "created_at",
+        ]
 
 
+class CreateActivitiesSerializer(serializers.ModelSerializer):
+    categories = CategoriesSerializer(many=True, required=False)
+
+    class Meta:
+        model = Activity
+        exclude = ["posted_by"]
+
+    def create(self, validated_data):
+        posted_by = self.context["request"].user.account
+        categories_data = validated_data.pop("categories", [])
+
+        activity = Activity.objects.create(**validated_data, posted_by=posted_by)
+
+        for category_data in categories_data:
+            category, created = Category.objects.get_or_create(
+                name=category_data["name"]
+            )
+            activity.categories.add(category)
+
+        return activity
+
+
+# ActivityRegistration Serializers
 class AdminActivityRegistrationSerializer(serializers.ModelSerializer):
-	username = serializers.ReadOnlyField(source='account.user.username')
+    username = serializers.ReadOnlyField(source="account.user.username")
 
-	class Meta:
-		model = ActivityRegistration
-		fields = '__all__'
+    class Meta:
+        model = ActivityRegistration
+        fields = "__all__"
 
 
 class NonAdminActivityRegistrationSerializer(serializers.ModelSerializer):
-	is_payed = serializers.ReadOnlyField()
-	status = serializers.ReadOnlyField()
-	activity = ListActivitiesSerializer
+    is_payed = serializers.ReadOnlyField()
+    status = serializers.ReadOnlyField()
+    activity = ListActivitiesSerializer
 
-	class Meta:
-		model = ActivityRegistration
-		exclude = ['account']
+    class Meta:
+        model = ActivityRegistration
+        exclude = ["account"]
 
-	def create(self, validated_data):
-		# Get the user's account from the request
-		account = self.context['request'].user.account
+    def create(self, validated_data):
+        # Get the user's account from the request
+        account = self.context["request"].user.account
 
-		# Try to get an existing ActivityRegistration or create a new one if it doesn't exist
-		activity_registration, created = ActivityRegistration.objects.get_or_create(
-			account=account,
-			activity=validated_data['activity']
-		)
+        # Try to get an existing ActivityRegistration or create a new one if it doesn't exist
+        activity_registration, created = ActivityRegistration.objects.get_or_create(
+            account=account, activity=validated_data["activity"]
+        )
 
-		return activity_registration
-	
+        return activity_registration
--- activities/views.py
+++ activities/views.py
@@ -1,93 +1,144 @@
-from rest_framework import viewsets
-from rest_framework.decorators import action,permission_classes
-from authentication.permissions import IsAdminOrReadOnly,IsAdminUser
+from authentication.permissions import IsAdminOrReadOnly, IsAdminUser
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.response import Response
+from rest_framework.decorators import action
+from rest_framework import viewsets, filters
 from rest_framework import status
 
+from django.db.models import Count
+
 from .permissions import IsObjectOwnerOrAdminPermission
-from .models import Category,Activity,ActivityRegistration
-from .serializers import CategoriesSerializer,DetailActivitiesSerializer,ListActivitiesSerializer,NonAdminActivityRegistrationSerializer,AdminActivityRegistrationSerializer
+from .models import Category, Activity, ActivityRegistration
+from .serializers import (
+    CategoriesSerializer,
+    DetailActivitiesSerializer,
+    ListActivitiesSerializer,
+    NonAdminActivityRegistrationSerializer,
+    AdminActivityRegistrationSerializer,
+    CreateActivitiesSerializer,
+    UpdateActivitiesSerializer,
+)
 
 
 class CategoriesViewSet(viewsets.ModelViewSet):
-	queryset = Category.objects.all()
-	serializer_class = CategoriesSerializer
-	permission_classes = [IsAdminOrReadOnly]
+    queryset = Category.objects.all()
+    serializer_class = CategoriesSerializer
+    permission_classes = [IsAdminOrReadOnly]
+    filter_backends = [DjangoFilterBackend, filters.OrderingFilter]
+    filterset_fields = ["name"]
+    ordering_fields = ["popular"]
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        queryset = queryset.annotate(popular=Count("activities"))
+        return queryset
 
 
 class ActivityViewSet(viewsets.ModelViewSet):
-	queryset = Activity.objects.all()
-	permission_classes = [IsAdminOrReadOnly]
-	serializer_class = ListActivitiesSerializer
-	filter_backends = [DjangoFilterBackend]
-	filterset_fields = ['title','categories__name', 'is_free']
+    queryset = Activity.objects.all()
+    permission_classes = [IsAdminOrReadOnly]
+    serializer_class = ListActivitiesSerializer
+    filter_backends = [DjangoFilterBackend, filters.OrderingFilter]
+    filterset_fields = ["title", "categories__name", "is_free"]
+    ordering_fields = ["created_at", "likes_count"]
 
-	def get_serializer_class(self):
-		if self.action == 'list':
-			return ListActivitiesSerializer
-		return DetailActivitiesSerializer
-	
-	@action(detail=True, methods=['POST'], url_name='toogle-like', permission_classes=[IsAdminUser])
-	def toogle_like(self, request, pk=None):
-		activity = self.get_object()
-		account = request.user.account
-		activity.toogle_like(account)
-		return Response({"is_liked": activity.isLikedBy(request.user.account)})
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        queryset = queryset.annotate(likes_count=Count("likes"))
+        return queryset
 
-	
+    def get_serializer_class(self):
+        if self.action == "list":
+            return ListActivitiesSerializer
+
+        if self.action in ["update", "partial_update"]:
+            print("update serializer")
+            return UpdateActivitiesSerializer
+
+        if self.action in ["create"]:
+            return CreateActivitiesSerializer
+
+        return DetailActivitiesSerializer
+
+    @action(
+        detail=True,
+        methods=["POST"],
+        url_name="toogle-like",
+        permission_classes=[IsAdminUser],
+    )
+    def toogle_like(self, request, pk=None):
+        activity = self.get_object()
+        account = request.user.account
+        activity.toogle_like(account)
+        return Response({"is_liked": activity.isLikedBy(request.user.account)})
 
 
 class ActivityRegistrationViewSet(viewsets.ModelViewSet):
-	queryset = ActivityRegistration.objects.all()
-	serializer_class = NonAdminActivityRegistrationSerializer
-	permission_classes = [IsObjectOwnerOrAdminPermission]
+    queryset = ActivityRegistration.objects.all()
+    serializer_class = NonAdminActivityRegistrationSerializer
+    permission_classes = [IsObjectOwnerOrAdminPermission]
 
+    def get_queryset(self):
+        user = self.request.user
+        if user.is_admin:
+            return ActivityRegistration.objects.all()
+        else:
+            return ActivityRegistration.objects.filter(account=user.account)
 
-	def get_queryset(self):
-		user = self.request.user
-		if user.is_admin:
-			return ActivityRegistration.objects.all()
-		else:
-			return ActivityRegistration.objects.filter(account=user.account)
+    def get_serializer_class(self):
+        if self.request.user.is_admin:
+            return AdminActivityRegistrationSerializer
 
+        return NonAdminActivityRegistrationSerializer
 
-	def get_serializer_class(self):
-		if self.request.user.is_admin:
-			return AdminActivityRegistrationSerializer
-		
-		return NonAdminActivityRegistrationSerializer
+    def update(self, request, *args, **kwargs):
+        if not self.request.user.is_admin:
+            return Response(
+                {
+                    "detail": "Non-admin users are not allowed to update ActivityRegistrations."
+                },
+                status=status.HTTP_403_FORBIDDEN,
+            )
 
+        return super().update(request, *args, **kwargs)
 
-	def update(self, request, *args, **kwargs):
-		if not self.request.user.is_admin:
-			return Response(
-                {"detail": "Non-admin users are not allowed to update ActivityRegistrations."},
-				status=status.HTTP_403_FORBIDDEN
-			)
-		
-		return super().update(request, *args, **kwargs)
+    @action(
+        detail=True,
+        methods=["POST"],
+        url_name="accept",
+        permission_classes=[IsAdminUser],
+    )
+    def accept(self, request, pk=None):
+        activity_registration = self.get_object()
+        activity_registration.acceptActivitieRegistration()
+        return Response({"status": activity_registration.status})
 
-	@action(detail=True, methods=['POST'], url_name='accept', permission_classes=[IsAdminUser])
-	def accept(self, request, pk=None):
-		activity_registration = self.get_object()
-		activity_registration.acceptActivitieRegistration()
-		return Response({"status": activity_registration.status})
+    @action(
+        detail=True,
+        methods=["POST"],
+        url_name="reject",
+        permission_classes=[IsAdminUser],
+    )
+    def reject(self, request, pk=None):
+        activity_registration = self.get_object()
+        activity_registration.rejectActivitieRegistration()
+        return Response({"status": activity_registration.status})
 
-	@action(detail=True, methods=['POST'], url_name='reject', permission_classes=[IsAdminUser])
-	def reject(self, request, pk=None):
-		activity_registration = self.get_object()
-		activity_registration.rejectActivitieRegistration()
-		return Response({"status": activity_registration.status})
-	
-	@action(detail=True, methods=['POST'], url_name='pay', permission_classes=[IsAdminUser])
-	def pay(self, request, pk=None):
-		activity_registration = self.get_object()
-		activity_registration.payRegistration()
-		return Response({"is_payed": activity_registration.is_payed})
+    @action(
+        detail=True, methods=["POST"], url_name="pay", permission_classes=[IsAdminUser]
+    )
+    def pay(self, request, pk=None):
+        activity_registration = self.get_object()
+        activity_registration.payRegistration()
+        return Response({"is_payed": activity_registration.is_payed})
 
-	@action(detail=True, methods=['POST'], url_name='unpay', permission_classes=[IsAdminUser])
-	def unpay(self, request, pk=None):
-		activity_registration = self.get_object()
-		activity_registration.unPayRegistration()
-		return Response({"is_payed": activity_registration.is_payed})
+    @action(
+        detail=True,
+        methods=["POST"],
+        url_name="unpay",
+        permission_classes=[IsAdminUser],
+    )
+    def unpay(self, request, pk=None):
+        activity_registration = self.get_object()
+        activity_registration.unPayRegistration()
+        return Response({"is_payed": activity_registration.is_payed})
