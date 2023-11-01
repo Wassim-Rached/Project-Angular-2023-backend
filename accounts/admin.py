4ae8ebf42e2e98b18a0213ea7bd1898166cc7251
Wassim-Rached <wassimrached404@gmail.com>
2023-11-01 20:35:08 +0100

    add "JoinClubForm" to admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import CustomUser,Account
+from .models import CustomUser, Account, JoinClubForm
 
 # Register your models here.
 admin.site.register(Account)
 admin.site.register(CustomUser)
+admin.site.register(JoinClubForm)
