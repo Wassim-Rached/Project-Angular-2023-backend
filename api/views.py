e6895d4a95f85a9c42b173fdcb61b1aa49b97ed5
Wassim-Rached <wassimrached404@gmail.com>
2023-11-10 21:00:40 +0100

    Add logging to AccountPhotoView

--- api/views.py
+++ api/views.py
@@ -5,14 +5,22 @@ from accounts.models import Account
 from activities.models import Activity
 
 
+import logging
+
+
 class AccountPhotoView(APIView):
     def get(self, request, pk):
         try:
             instance = Account.objects.get(pk=pk)
             image_path = instance.photo.path
+            logging.info(f"Image path: {image_path}")
             return FileResponse(open(image_path, "rb"))
         except Account.DoesNotExist:
-            return Response({"error": "MyModel not found"}, status=404)
+            logging.error(f"Account with pk={pk} does not exist.")
+            return Response({"error": "Account not found"}, status=404)
+        except Exception as e:
+            logging.error(f"An error occurred: {e}")
+            return Response({"error": "An error occurred"}, status=500)
 
 
 class ActivityPhotoView(APIView):
