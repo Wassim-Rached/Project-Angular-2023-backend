999e5f2a04191b2f6928fd2cb5613aa296cae9f2
Wassim-Rached <wassimrached404@gmail.com>
2023-11-18 14:32:24 +0100

    Add healthcheck endpoint to API

--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
 
-from .views import hello_world
+from .views import hello_world, healthcheck_view
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', hello_world,name="hello-world"),
-    path('api/',include('api.urls'))
+    path("admin/", admin.site.urls),
+    path("", hello_world, name="hello-world"),
+    path("api/", include("api.urls")),
+    path("api/healthcheck/", healthcheck_view, name="healthcheck"),  # Add this line
 ]
