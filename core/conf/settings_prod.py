b668ceea3ff4ac95890869b77824d5896d961750
Wassim-Rached <wassimrached404@gmail.com>
2023-11-19 22:38:53 +0100

    Update token lifetimes in settings_prod.py

--- core/conf/settings_prod.py
+++ core/conf/settings_prod.py
@@ -179,7 +179,7 @@ CORS_ALLOW_HEADERS = [
 from datetime import timedelta
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(weeks=48),
+    "ACCESS_TOKEN_LIFETIME": None,
     "REFRESH_TOKEN_LIFETIME": timedelta(weeks=96),
     "ROTATE_REFRESH_TOKENS": False,
     "BLACKLIST_AFTER_ROTATION": False,
@@ -202,8 +202,8 @@ SIMPLE_JWT = {
     "TOKEN_USER_CLASS": "rest_framework_simplejwt.models.TokenUser",
     "JTI_CLAIM": "jti",
     "SLIDING_TOKEN_REFRESH_EXP_CLAIM": "refresh_exp",
-    "SLIDING_TOKEN_LIFETIME": timedelta(minutes=5),
-    "SLIDING_TOKEN_REFRESH_LIFETIME": timedelta(days=1),
+    "SLIDING_TOKEN_LIFETIME": None,
+    "SLIDING_TOKEN_REFRESH_LIFETIME": timedelta(weeks=48),
     "TOKEN_OBTAIN_SERIALIZER": "rest_framework_simplejwt.serializers.TokenObtainPairSerializer",
     "TOKEN_REFRESH_SERIALIZER": "rest_framework_simplejwt.serializers.TokenRefreshSerializer",
     "TOKEN_VERIFY_SERIALIZER": "rest_framework_simplejwt.serializers.TokenVerifySerializer",
